eab9b1b79f24faa9f38a7d0f26468158ee466223
zyc <392318709@qq.com>
2018-06-27 18:24:21 +0800

    管理后台 - 新增、变价商品

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -42,7 +42,7 @@ class ProductController extends Controller
         return Admin::content(function (Content $content) use ($id) {
 
             $content->header('header');
-            $content->description('description');
+            $content->description('编辑商品');
 
             $content->body($this->form()->edit($id));
         });
@@ -58,7 +58,7 @@ class ProductController extends Controller
         return Admin::content(function (Content $content) {
 
             $content->header('header');
-            $content->description('description');
+            $content->description('创建商品');
 
             $content->body($this->form());
         });
@@ -82,7 +82,7 @@ class ProductController extends Controller
             $grid->on_sale('已上架')->display(function($value){
                 return $value ? '是' : '否';
             });
-            $grid->created_at();
+            $grid->created_at('创建时间');
         });
     }
 
@@ -96,9 +96,25 @@ class ProductController extends Controller
         return Admin::form(Product::class, function (Form $form) {
 
             $form->display('id', 'ID');
+            $form->text('title', '商品名称')->rules('required');
+            // 创建一个选择图片的框
+            $form->image('image', '封面图片')->rules('required|image');
+            // 创建一个富文本编辑器
+            $form->editor('description', '商品描述')->rules('required');
+            // 创建一组单选框
+            $form->radio('on_sale', '上架')->options(['1' => '是', '0'=> '否'])->default('0');
+            // 直接添加一对多的关联模型
+            $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
+                $form->text('title', 'SKU 名称')->rules('required');
+                $form->text('description', 'SKU 描述')->rules('required');
+                $form->text('price', '单价')->rules('required|numeric|min:0.01');
+                $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+            });
 
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            // 定义事件回调，当模型即将保存时会触发这个回调
+            $form->saving(function (Form $form) {
+                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+            });
         });
     }
 }
